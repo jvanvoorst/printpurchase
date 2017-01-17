212f2aeb363658317535128208f48f658a7d0909
justin.vanvoorst@gmail.com <justin.vanvoorst@gmail.com>
2017-01-17 16:38:38 -0700

    create db.php

--- resources/db.php
+++ resources/db.php
@@ -2,18 +2,10 @@
 
 include('./../resources/config.php');
 
-$query = $db->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_TYPE='BASE TABLE'");
-$tables = $query->fetchAll(PDO::FETCH_COLUMN);
-
-if (empty($tables)) {
-    echo "<p>There are no tables in database \"{$database}\".</p>";
-} else {
-    echo "<p>Database \"{$config['db']['dbName']}\" has the following tables:</p>";
-    echo "<ul>";
-    foreach ($tables as $table) {
-        echo "<li>{$table}</li>";
-    }
-    echo "</ul>";
-}
+$db = new PDO(
+	"mysql:host={$config["db"]["host"]};dbname={$config["db"]["dbName"]}",
+	$config["db"]["userName"],
+	$config["db"]["password"]
+);
 
 ?>
