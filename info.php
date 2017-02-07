f3def9e293ef2351039c60f4aa03dbedc87adc82
justin.vanvoorst@gmail.com <justin.vanvoorst@gmail.com>
2017-02-07 14:57:08 -0700

    remove noorder

--- info.php
+++ info.php
@@ -5,12 +5,13 @@
 </head>
 <body>
 
-<p> <b>uid </b> <?php echo $_SERVER['uid']; ?> </p>
-<p> <b>givenName </b> <?php echo $_SERVER['givenName']; ?> </p>
-<p> <b>mail </b> <?php echo $_SERVER['mail']; ?> </p>
-<p> <b>sn </b> <?php echo $_SERVER['sn']; ?> </p>
-<p> <b>eduPersonAffiliation </b> <?php echo $_SERVER['eduPersonAffiliation']; ?> </p>
-<p> <b>cuEduPersonHomeDepartment </b> <?php echo $_SERVER['cuEduPersonHomeDepartment']; ?> </p>
+
+
+<?php
+	include('./../resources/config.php');
+	echo $config['db']['host'];
+	echo 'something else';
+?>
 
 </body>
 </html>
