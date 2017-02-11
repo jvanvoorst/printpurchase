919200adee1d51746f96036e8c66391c562c65f1
justin.vanvoorst@gmail.com <justin.vanvoorst@gmail.com>
2017-02-10 17:50:20 -0700

    add gulp deploy

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+	rsync = require('gulp-rsync'),
+	gutil = require('gulp-util');
+
+gulp.task('deploy', function() {
+
+		conf = {
+		progress: true,
+		incremental: true,
+		recursive: true,
+		emptyDirectories: true,
+		root: 'src',
+		hostname: 'culibraries01.colorado.edu',
+		username: 'vanvoors',
+		destination: '/data/web/htdocs/culibraries/printpurchase',
+	};
+
+	return gulp.src('src/')
+		.pipe(rsync(conf));
+});
+
+function throwError(taskName, msg) {
+  throw new gutil.PluginError({
+      plugin: taskName,
+      message: msg
+    });
+}
+
